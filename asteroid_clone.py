c8d61094503b957abc077dba4acdb0e834e864b5
CAW Busteed <acedric1@gmail.com>
2023-01-08 17:15:51 -0500

    rect vs surface

--- asteroid_clone.py
+++ asteroid_clone.py
@@ -24,30 +24,82 @@ BG = pygame.image.load('Assets/starry_sky_bg.jpg')
 BG = pygame.transform.scale(BG, (SW, SH))
 
 
-class Player(object):
+class Player(pygame.sprite.Sprite):
 
     def __init__(self):
-        self.img = PLAYER_ICON
-        self.w = self.img.get_width()
-        self.h = self.img.get_height()
+        super(Player, self).__init__()
+        self.surf = PLAYER_ICON.convert()
+        self.surf.set_colorkey((255, 255, 255), RLEACCEL)
+        self.rect = self.surf.get_rect()
+        # self.w = self.rect.get_width()
+        # self.h = self.rect.get_height()
         #position shortcut?
         self.x = SW // 2
         self.y = SH // 2
+        self.angle = 0
+        #supposed to determine rotation of player
+        self.rotatedSurf = pygame.transform.rotate(self.rect, self.angle)
+        #properly centers
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        #using math to properly set parameters
+        self.cosine = math.cos(math.radians(self.angle + 90))
+        self.sine = math.sin(math.radians(self.angle + 90))
+        #gives us x and y of front ship to determine projectile direction when shot
+        self.head = (self.x + self.cosine * self.w//2, self.y - self.sine * self.h//2)
 
-    # TODO: change movement to orientation
     def draw(self, win):
-        win.blit(self.img, [self.x, self.y, self.w, self.h])
+        #win.blit(self.rect, [self.x, self.y, self.w, self.h])
+        #the following will track sprite turn
+        win.blit(self.rotatedSurf, self.rotatedRect)
+    
+    def turnLeft(self):
+        self.angle += 5
+        self.rotatedSurf = pygame.transform.rotate(self.rect, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle+90))
+        self.sine = math.sin(math.radians(self.angle+90))
+        #to reset. The turtorial had a + \/ rather than a *, could he have made a msitake?
+        self.head = (self.x + self.cosine + self.w//2, self.y - self.sine * self.h//2)
+
+
+    def turnRight(self):
+        self.angle -= 5
+        self.rotatedSurf = pygame.transform.rotate(self.rect, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle+90))
+        self.sine = math.sin(math.radians(self.angle+90))
+        #to reset
+        self.head = (self.x + self.cosine + self.w//2, self.y - self.sine * self.h//2)
+
+    def moveForward(self):
+        self.x += self.cosine * 6
+        self.y -= self.sine * 6
+        self.rotatedSurf = pygame.transform.rotate(self.rect, self.angle)
+        self.rotatedRect = self.rotatedSurf.get_rect()
+        self.rotatedRect.center = (self.x, self.y)
+        self.cosine = math.cos(math.radians(self.angle+90))
+        self.sine = math.sin(math.radians(self.angle+90))
+        #to reset
+        self.head = (self.x + self.cosine + self.w//2, self.y - self.sine * self.h//2)
+        #inverse to decvelerate?
+
 
     #copy-pasted from simple, need to figure out how to orient it
-    def update(self, pressed_keys):
-        if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
-        if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
-        if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
-        if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+    # def update(self, pressed_keys):
+    #     if pressed_keys[K_UP]:
+    #         player.moveForward()
+    #     #for deceleration it would be player.Brake
+    #     # if pressed_keys[K_DOWN]:
+    #     #    self.rect.move_ip(0, 5)
+    #     if pressed_keys[K_LEFT]:
+    #         player.turnLeft()
+    #     if pressed_keys[K_RIGHT]:
+    #         player.turnRight()
+
+                
 
 
 SCREEN = pygame.display.set_mode((SW, SH))
@@ -83,7 +135,27 @@ while run:
     clock.tick(60)
     if not gameover:
         #pass acts as a placeholder. Neat trick!
-        pass
+        #already imported controls
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.turnLeft()
+        if keys[pygame.K_RIGHT]:
+            player.turnRight()
+        if keys[pygame.K_UP]:
+            player.moveForward()
+        # translate coordinates to keep player in bounds
+        #OR make it so screen follows player
+        if player.rect.left < -10:
+            player.rect.left = -10
+        # if player.rect.right > SW:
+        #     player.rect.right = SW
+        # if player.rect.top <= -10:
+        #     player.rect.top = -10
+        # if player.rect.bottom >= SH:
+        #     player.rect.bottom = SH
+    
+
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -96,8 +168,11 @@ while run:
             # If the Esc key is pressed, then exit the main loop
             if event.key == K_ESCAPE:
                 run = False
-            elif event.key == K_RIGHT:
-                player.img= pygame.transform.rotate(PLAYER_ICON, -90).convert()
+            
+            #elif event.key == K_RIGHT:
+            #    player.img= pygame.transform.rotate(PLAYER_ICON, -90).convert()
+            #elif event.key == K_LEFT:
+            #    player.img= pygame.transform.rotate(PLAYER_ICON, 90).convert()
         # Check for QUIT event. If QUIT, then set running to false.
         elif event.type == QUIT:
             run = False
