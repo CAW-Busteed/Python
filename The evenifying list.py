6df71ca5315ca2a651dd1a748b158ae81c30af29
CAW Busteed <acedric1@gmail.com>
2023-01-23 17:57:44 -0500

    working rockpaperscissors

--- The evenifying list.py
+++ The evenifying list.py
@@ -5,7 +5,16 @@
 a = [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
 # evens = [ int % 2 == 0 for int in a]
 #this ends up with a series of true and flases I want numbers
-evens = [int for int in a if int % 2 == 0 ]
-thriples = [int for int in a if int % 3 == 0]
-print(evens)
-print(thriples)
+# evens = [int for int in a if int % 2 == 0 ]
+# thriples = [int for int in a if int % 3 == 0]
+# print(evens)
+# print(thriples)
+def evensx():
+    return [int for int in a if int % 2 == 0 ]
+
+def triplesx():
+    return [int for int in a if int % 3 == 0]
+
+result = evensx()
+result1 = triplesx()
+print(result, result1)
