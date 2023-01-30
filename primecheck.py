a9d8f933a3cecd74beedb7477e39a5eb8984a947
CAW Busteed <acedric1@gmail.com>
2023-01-30 17:42:08 -0500

    edits and thoughrs 1/30

---
+++ primecheck.py
@@ -0,0 +1,40 @@
+'''
+num = int(input('Give a number to check if it is a prime: '))
+
+
+
+i = [x for x in range(1, num)]
+if num % x == 0
+ check if number % i == 0:
+     yes:   print(number, ' is not a prime number.')
+    no:    print(number, ' is a prime number.')
+
+
+
+'''
+
+num = int(input('Give a number to check if it is a prime: '))
+i = [x for x in range(2, num) if num % x == 0]
+
+def check_prime(n):
+    if len(i) == 0:
+        print('Prime!')
+    else:
+        print('Not a prime!')
+        print(i)
+
+check_prime(num)
+
+
+
+
+# def prime(num):
+#     i = 2
+#     while i <= num/2:
+#         rem = num % i
+
+
+# # NUMBER = int(input('Give me a number: '))
+
+# prime(NUMBER)
+
