351f3ba8847d8bd7e3f8f97ee33fd807baa31893
CAW Busteed <acedric1@gmail.com>
2023-02-01 16:53:50 -0500

    efficient final

--- fibonacci sequ check.py
+++ fibonacci sequ check.py
@@ -6,10 +6,7 @@ def fibonacci(n):
     x, y = 1, 1
     fib_list = [x, y]
     for i in range(n-2):
-        x1, y1 = x, y # prep to swap
-        x = y1 #redefine x
-        y = x1 + y1 #redefine new y val
-        #add into list
+        x, y = y, (x+y) #swap
         fib_list.append(y)
     return fib_list
 
