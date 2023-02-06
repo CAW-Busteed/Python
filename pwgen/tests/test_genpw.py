6643dda1f1365b1e0b580e570083f3ad197b5865
CAW Busteed <acedric1@gmail.com>
2023-02-06 16:56:44 -0500

    password testing

--- pwgen/tests/test_genpw.py
+++ pwgen/tests/test_genpw.py
@@ -1,5 +1,13 @@
 from pwgen import genpw
 
-def test_pwgen():
-    assert genpw.ran_choice(1) == 'abc'
+def test_pwgen_shor():
+    x = len(genpw.pass_gen('short'))
+    assert x <= 9 
 
+def test_pwgen_med():
+    x = len(genpw.pass_gen('medium'))
+    assert (x >= 12 and x <= 18) 
+
+def test_pwgen_long():
+    x = len(genpw.pass_gen('long'))
+    assert (x >= 20 and x <= 32)
