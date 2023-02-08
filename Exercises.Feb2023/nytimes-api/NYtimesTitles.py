63923cb8f095ab4b687817df81f586c294a6536c
CAW Busteed <acedric1@gmail.com>
2023-02-08 17:28:01 -0500

    advanced but simplified for cedric

--- Exercises.Feb2023/nytimes-api/NYtimesTitles.py
+++ Exercises.Feb2023/nytimes-api/NYtimesTitles.py
@@ -10,11 +10,16 @@ print titles on request
 import requests
 from bs4 import BeautifulSoup
 
+#base url to grab
+base_url = 'http://www.nytimes.com/'
+
 #fetching from site
-r = requests.get('https://www.nytimes.com/')
+r = requests.get(base_url)
 
-#this should parse r into  HTML
-SOUP = BeautifulSoup(r, 'html.parser')
+# #this should parse r into  HTML
+SOUP = BeautifulSoup(r.text, features="html.parser")
+
+for x in SOUP.find_all(class_=["indicate-hover"]):
+    print(f"- {x.text}")
 
 
-print(SOUP.prettify())
