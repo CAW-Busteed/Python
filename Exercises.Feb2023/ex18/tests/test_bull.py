4766b4eb61dcb625cb051371523183ebe50ab5a2
CAW Busteed <acedric1@gmail.com>
2023-02-13 17:14:36 -0500

    saved cows

--- Exercises.Feb2023/ex18/tests/test_bull.py
+++ Exercises.Feb2023/ex18/tests/test_bull.py
@@ -6,20 +6,20 @@ from ex18.bull import compare
 def test_compare_ok():
     a = [1, 2, 3, 4]
     b = [3, 2, 3, 3]
-    assert compare(a,b) == [2]
+    assert compare(a,b) == [2, 0]
     a = [1, 3, 4, 4]
     b = [3, 3, 3, 3]
-    assert compare(a,b) == [1]
+    assert compare(a,b) == [1, 0]
     a = [1, 2, 3, 4]
     b = [3, 2, 4, 3]
-    assert compare(a,b) == [3]
+    assert compare(a,b) == [3, 0]
     a = [1, 2, 3, 4]
     b = [5, 6, 7, 8]
-    assert compare(a,b) == [0]
+    assert compare(a,b) == [0, 0]
 
 def test_compare_fail():
     a = [1, 2, 3, 4]
     b = [3, 6, 8, 7]
-    assert compare(a,b) == 1
+    assert compare(a,b) == [1, 0]
     b = [4, 5, 3, 7]
-    assert compare(a,b) == 2
+    assert compare(a,b) == [2, 0]
