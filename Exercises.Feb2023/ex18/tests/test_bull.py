9c5d85e7605a336c4538b6c6a9e8ead54f2e9d15
CAW Busteed <acedric1@gmail.com>
2023-02-15 17:33:41 -0500

    revamped test

--- Exercises.Feb2023/ex18/tests/test_bull.py
+++ Exercises.Feb2023/ex18/tests/test_bull.py
@@ -1,4 +1,3 @@
-from ex18.bull import compare
 from ex18.bull import cowbulls
 from ex18.bull import bulls_id
 from ex18.bull import cows_id
@@ -25,27 +24,32 @@ def test_cows_id():
     b = [3, 2, 3, 3]
     assert cows_id(a,b) == [2, 3]
 
-
-def test_compare_ok():
-    a = [1, 2, 3, 4]
+def test_not_cows():
+    a = [1, 4, 3, 2]
     b = [3, 2, 3, 3]
-    assert compare(a,b) == [1, 1]
-    #TODO: Correct answer would be [0, 2]
-    a = [1, 3, 4, 4]
-    b = [3, 3, 3, 3]
-    assert compare(a,b) == [1, 0]
-    #TODO: Correct answer would be [1, 0]
-    a = [1, 2, 3, 4]
-    b = [3, 2, 4, 3]
-    assert compare(a,b) == [2, 1]
-    a = [1, 2, 3, 4]
-    b = [5, 6, 7, 8]
-    assert compare(a,b) == [0, 0]
+    assert not_cows(a,b) == [[1, 3], [4, 2], [2, 3]]
+
+
+# def test_compare_ok():
+#     a = [1, 2, 3, 4]
+#     b = [3, 2, 3, 3]
+#     assert compare(a,b) == [1, 1]
+#     #TODO: Correct answer would be [0, 2]
+#     a = [1, 3, 4, 4]
+#     b = [3, 3, 3, 3]
+#     assert compare(a,b) == [1, 0]
+#     #TODO: Correct answer would be [1, 0]
+#     a = [1, 2, 3, 4]
+#     b = [3, 2, 4, 3]
+#     assert compare(a,b) == [2, 1]
+#     a = [1, 2, 3, 4]
+#     b = [5, 6, 7, 8]
+#     assert compare(a,b) == [0, 0]
     
 
-def test_compare_fail():
-    a = [1, 2, 3, 4]
-    b = [3, 6, 8, 7]
-    assert compare(a,b) == [1, 0]
-    b = [4, 5, 3, 7]
-    assert compare(a,b) == [1, 1]
+# def test_compare_fail():
+#     a = [1, 2, 3, 4]
+#     b = [3, 6, 8, 7]
+#     assert compare(a,b) == [1, 0]
+#     b = [4, 5, 3, 7]
+#     assert compare(a,b) == [1, 1]
