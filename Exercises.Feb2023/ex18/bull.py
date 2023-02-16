bc4ebbaec0659b04f4e9898847d4f0542792b479
Roy Mathew <rmathew8@gmail.com>
2023-02-16 09:26:41 -0500

    optimized the code somewhat. Worth pondering the differences.

--- Exercises.Feb2023/ex18/bull.py
+++ Exercises.Feb2023/ex18/bull.py
@@ -13,90 +13,70 @@ generated (n) is array of digits
 '''
 import random
 
-# TODO: rename i, j to a, b in all funcs
 
-def get_cows(a,b):
+def get_cows(actual, guess):
     """
-    this function computes the # of cows, returns a list..
+    this function computes the # of cows, returns actual list of cows
+    and actual reduced version (with cows removed) of the input arrays
     """
-    cows = []
-    for i, j in zip(a, b):
+    cows, bs1, bs2 = [], [], []
+    for i, j in zip(actual, guess):
         if i == j:
-            cows.append(i)   
-    return cows
+            cows.append(i)
+        else:
+            bs1.append(i)
+            bs2.append(j)
+    return cows, [bs1, bs2]
 
-def get_not_cows(i,j):
-    notcows = []
-    for a,b in zip(i,j):
-        if a != b:
-            notcows.append([a, b])
-    return notcows
 
-def get_bulls(i, j):
-    bulls = [x for x in i if x in j]
-    return bulls
-    
-def cowbulls(a, b):
-    cows_ = get_cows(a,b)
-    pbulls_ = get_not_cows(a,b)
+def get_bulls(actual, guess):
+    "find matching bulls"
+    return [x for x in actual if x in guess]
 
-    # transpose a matrix
-    pbulls1, pbulls2 = [], []
-    for _a,_b in pbulls_:
-        pbulls1.append(_a)
-        pbulls2.append(_b)
 
-    bulls_ = get_bulls(pbulls1, pbulls2)
-    return [cows_, bulls_]
+def get_cows_and_bulls(actual, guess):
+    cows, [bs1, bs2] = get_cows(actual, guess)
+    return [cows, get_bulls(bs1, bs2)]
 
-        
 
 #main loop
-if __name__=="__main__":
+if __name__ == "__main__":
     # generate a random 4-digit num.
-    num_ = [random.randint(0,9), random.randint(0,9), random.randint(0,9), random.randint(0,9)]
-    tally = 0 # tally is how many user attempts
+    num_ = [
+        random.randint(0, 9),
+        random.randint(0, 9),
+        random.randint(0, 9),
+        random.randint(0, 9)
+    ]
+
+    tally = 0  # tally is how many user attempts
     #run = True # flag to terminate main loop.
 
     #explanation and start of loop
-    print('A return of a cow means a correct number and correct place. A bull is a correct guess in the wrong place.')
+    print(
+        'A return of a cow means a correct number and correct place. A bull is a correct guess in the wrong place.'
+    )
     while True:
-            #user input
-            ans_ = input('Guess a 4 digit number: ')
+        #user input
+        ans_ = input('Guess a 4 digit number: ')
+        #exit condition
+        if ans_ == 'quit':
+            break
 
-            #exit condition
-            if ans_ == 'quit':
-                break
-            
-            #transform input into list
-            trial = [int(x) for x in ans_]
+        #transform input into list
+        trial = [int(x) for x in ans_]
 
-            # use cowbulls function to compare
-            count_ = cowbulls(num_,trial)
+        # use cowbulls function to compare
+        count_ = get_cows_and_bulls(num_, trial)
 
-            #add to tally
-            tally+=1
+        #add to tally
+        tally += 1
 
-            #tell user their score
-            print(f'You have {len(count_[0])} cows and {len(count_[1])} bulls.')
+        #tell user their score
+        print(f'You have {len(count_[0])} cows and {len(count_[1])} bulls.')
 
-            if len(count_[0]) == 4:
-                print(f'All cows, you win! You guessed {tally} times')
-                break
-            else:
-                print('Try again!')
-
-
-
-
-
-    
-
-            
-
-
-
-    
-    
-
-
+        if len(count_[0]) == 4:
+            print(f'All cows, you win! You guessed {tally} times')
+            break
+        else:
+            print('Try again!')
--- Exercises.Feb2023/ex18/tests/test_bull.py
+++ Exercises.Feb2023/ex18/tests/test_bull.py
@@ -1,55 +1,23 @@
-from ex18.bull import cowbulls
+from ex18.bull import get_cows_and_bulls
 from ex18.bull import get_bulls
 from ex18.bull import get_cows
-from ex18.bull import get_not_cows
 
-
-# def test_df():
-#     assert add.add(1, 2) == 3
-
-def test_cowbulls():
-    a = [1, 2, 3, 4]
-    b = [4, 2, 3, 3]
-    assert cowbulls(a,b) == [[2, 3], [4]]
-
-def test_get_bulls():
-    a = [1, 4, 3, 2]
-    b = [3, 2, 3, 3]
-    assert get_bulls(a,b) == [3, 2]
-    b = [2, 8, 7, 9]
-    assert get_bulls(a,b) == [2]
 
 def test_get_cows():
     a = [1, 2, 3, 4]
     b = [3, 2, 3, 3]
-    assert get_cows(a,b) == [2, 3]
+    assert get_cows(a, b) == ([2, 3], [[1, 4], [3, 3]])
 
-def test_get_not_cows():
+
+def test_get_bulls():
     a = [1, 4, 3, 2]
     b = [3, 2, 3, 3]
-    assert get_not_cows(a,b) == [[1, 3], [4, 2], [2, 3]]
+    assert get_bulls(a, b) == [3, 2]
+    b = [2, 8, 7, 9]
+    assert get_bulls(a, b) == [2]
 
 
-# def test_compare_ok():
-#     a = [1, 2, 3, 4]
-#     b = [3, 2, 3, 3]
-#     assert compare(a,b) == [1, 1]
-#     #TODO: Correct answer would be [0, 2]
-#     a = [1, 3, 4, 4]
-#     b = [3, 3, 3, 3]
-#     assert compare(a,b) == [1, 0]
-#     #TODO: Correct answer would be [1, 0]
-#     a = [1, 2, 3, 4]
-#     b = [3, 2, 4, 3]
-#     assert compare(a,b) == [2, 1]
-#     a = [1, 2, 3, 4]
-#     b = [5, 6, 7, 8]
-#     assert compare(a,b) == [0, 0]
-    
-
-# def test_compare_fail():
-#     a = [1, 2, 3, 4]
-#     b = [3, 6, 8, 7]
-#     assert compare(a,b) == [1, 0]
-#     b = [4, 5, 3, 7]
-#     assert compare(a,b) == [1, 1]
+def test_cows_and_bulls():
+    a = [1, 2, 3, 4]
+    b = [4, 2, 3, 3]
+    assert get_cows_and_bulls(a, b) == [[2, 3], [4]]
