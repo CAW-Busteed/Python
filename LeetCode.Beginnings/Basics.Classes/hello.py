56cb27c6459dcf1e52b574ce16bf4ed3b779abdd
Roy Mathew <rmathew8@gmail.com>
2023-02-26 12:10:03 -0500

    added basics of classes.

---
+++ LeetCode.Beginnings/Basics.Classes/hello.py
@@ -0,0 +1,20 @@
+class Hello():
+
+    def __init__(self, name, age=24):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, I am {self.name}; I am {self.age} years old")
+
+
+patrick = Hello('Patrick', age=60)
+patrick.greet()
+
+cedric = Hello('Cedric')
+cedric.greet()
+
+h2 = Hello('Viviane', age=59)
+h2.greet()
+
+# h1 = Hello()
