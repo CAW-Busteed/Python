6c56885c0e9489d4fb1cd86b1373f234bfbe8b3a
CAW Busteed <acedric1@gmail.com>
2023-02-28 17:14:04 -0500

    committed

--- LeetCode.Beginnings/Basics.Classes/tests/test_diction.py
+++ LeetCode.Beginnings/Basics.Classes/tests/test_diction.py
@@ -1,6 +1,11 @@
 from dictionary_ex import pull_rans
 
 def test_pull_rans():
-    sample_text = "bana"
-    message = "banana"
-    assert pull_rans(sample_text, message) == False
+    MAGAZINE = "bana"
+    RANSOM = "banana"
+    assert pull_rans(MAGAZINE,RANSOM) == False
+
+def test_take_rans():
+    MAGAZINE = "banana"
+    RANSOM = "banna"
+    assert pull_rans(MAGAZINE, RANSOM) == True
