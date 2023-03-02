5375af434363a2da434375e040fca8d8316a4134
Roy Mathew <rmathew8@gmail.com>
2023-03-02 05:48:26 -0500

    renamed directory so it doesn't break imports

--- LeetCode.Beginnings/lcp/lcp.py
+++ LeetCode.Beginnings/lcp/lcp.py
@@ -12,15 +12,14 @@ function get_LCP(input)
     print pos
 '''
 
+
 def get_lcp(_input):
-    pos =0
+    pos = 0
     for c in _input[0]:
         for word in _input[1:]:
             if c == word[pos]:
                 pass
             else:
                 break
-            pos+=1
+            pos += 1
     return _input[0][:pos]
-        
-    
--- LeetCode.Beginnings/lcp/tests/test_lcp.py
+++ LeetCode.Beginnings/lcp/tests/test_lcp.py
@@ -1,5 +1,6 @@
 from lcp import get_lcp
 
+
 def test_get_lcp():
     strs = ['flower', 'flow', 'flair']
     assert get_lcp(strs) == 'fl'
