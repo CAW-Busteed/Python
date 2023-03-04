39881b3cce6b0424d559cc6ec57f8a1a9ea69570
CAW Busteed <acedric1@gmail.com>
2023-03-04 12:21:02 -0500

    added classification

--- Exercises.Feb2023/prob_calc/probability.py
+++ Exercises.Feb2023/prob_calc/probability.py
@@ -10,16 +10,35 @@ Procedure:
 '''
 
 
-
-test_list = [1, 2, 3, 4, 5, 5, 6, 3, 3, 3, 3, 7, 8, 9, 10, 11, 11, 12, 11, 14, 15, 16, 20, 20, 21]
+#test_list = [1, 2, 3, 4, 5, 5, 6, 3, 3, 3, 3, 7, 8, 9, 10, 11, 11, 12, 11, 14, 15, 16, 20, 20, 21]
 
 def probability_id(element_, list_):
     occur = list_.count(element_)
     return (occur / len(list_))*100
 
-user_choice = input('Give an element to check: ')
 
-result = probability_id(user_choice,test_list)
+# wouldn't it be better to define these in a dictionary?
+def res_classification(percentage):
+    if percentage >= 50:
+        return 'COMMON'
+    else:
+        if percentage >= 25:
+            return 'UNCOMMON'
+        else:
+            if percentage >= 7:
+                return 'RARE'
+            else:
+                if percentage >= 1:
+                    return 'EXOTIC'
+                else:
+                    if percentage >= 0.3:
+                        return 'LEGENDARY'
+                    else:
+                        return 'DIVINE'
 
-print(f'There is a' [result]'percent chance of this element appearing in the list.')
+# user_choice = input('Give an element to check: ')
+
+# result = probability_id(user_choice,test_list)
+
+# print(f'There is a' [result]'percent chance of this element appearing in the list.')
 
