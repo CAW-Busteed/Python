2474e5de8f9c9d9afda5c8531de5857b3a660920
CAW Busteed <acedric1@gmail.com>
2023-03-04 12:23:24 -0500

    fixed minor issues regarding m module names

--- LeetCode.Beginnings/least_common_prefix/tests/test_lcp.py
+++ LeetCode.Beginnings/least_common_prefix/tests/test_lcp.py
@@ -4,10 +4,10 @@ def test_get_lcp():
     strs = ['flower', 'flow', 'flair']
     assert get_lcp(strs) == 'fl'
 
-def fail_test_get_lcp():
+def test_fail_get_lcp():
     strs = ['vorpal', 'vanquish', 'vain']
     assert get_lcp(strs) == 'v'
 
-def fail_test2_get_lcp():
+def test_fail_2_get_lcp():
     strs = ['cat', 'dog', 'vain']
     assert get_lcp(strs) == ''
