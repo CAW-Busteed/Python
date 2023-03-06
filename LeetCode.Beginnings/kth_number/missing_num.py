073f1fec72b9be3bd47bbecad416dec89f4310fd
Roy Mathew <rmathew8@gmail.com>
2023-03-06 18:07:12 -0500

    updates.

--- LeetCode.Beginnings/kth_number/missing_num.py
+++ LeetCode.Beginnings/kth_number/missing_num.py
@@ -1,20 +1,23 @@
 '''
-missing<-[]
-#walk through list
-for number in sequence:
-    #if gap
-    for number in range from [current number] to [next number]
-        if number+1 is not equal to [next number]
-            #add missing numbers to list
-            add [number+1] to missing list
-    if the missing list is equal to the number of places requested, return [last number in missing list]
+missing <- []
+for idx, num in enumerated(arr[1:]):
+  a, b = num, next-num
+  if (b-a)>1:   
+    # gap exists in pair
+    add a+1 .. b-1 to missing    
 '''
+
+
 def find_lastmissingno(arr, k) -> int:
     missingno = []
-    for num in arr:
-        for nmueral in range(num,):
-            if nmueral+1 !=
-            missingno.append(nmueral)
-        if len(missingno) == k-1:
-            break
-    return missingno[k-1]
+    for idx, num in enumerate(arr[1:]):
+        a, b = arr[idx], arr[idx + 1]
+        if (b - a) > 1:
+            for i in range(a + 1, b):
+                missingno.append(i)
+    print(missingno)
+
+
+arr = [1, 2, 3, 5, 6, 9, 10, 11, 13, 17]
+k = 3
+find_lastmissingno(arr, k)
--- LeetCode.Beginnings/kth_number/tests/test_missing_num.py
+++ LeetCode.Beginnings/kth_number/tests/test_missing_num.py
@@ -1,6 +1,7 @@
-from missing_num import find_lastmissingno
+from kth_number.missing_num import find_lastmissingno
+
 
 def test_find_lastmissingno():
-    arr = [1,2,3,5,6,9,10,11,13,17]
-    k = 4
-    assert find_lastmissingno(arr,k) == 12
+    arr = [1, 2, 3, 5, 6, 9, 10, 11, 13, 17]
+    k = 3
+    assert find_lastmissingno(arr, k) == 12
