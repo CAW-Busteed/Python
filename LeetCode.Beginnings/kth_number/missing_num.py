fac140a45bb1ec6edfcdd7297d15127b25a7affe
CAW Busteed <acedric1@gmail.com>
2023-03-07 17:26:38 -0500

    work done so far March/7/23

--- LeetCode.Beginnings/kth_number/missing_num.py
+++ LeetCode.Beginnings/kth_number/missing_num.py
@@ -7,17 +7,19 @@ for idx, num in enumerated(arr[1:]):
     add a+1 .. b-1 to missing    
 '''
 
-
+#TODO come back and look at this, see if you understand
 def find_lastmissingno(arr, k) -> int:
     missingno = []
     for idx, num in enumerate(arr[1:]):
         a, b = arr[idx], arr[idx + 1]
         if (b - a) > 1:
             for i in range(a + 1, b):
-                missingno.append(i)
-    print(missingno)
+                if i >= 0:
+                    missingno.append(i)
+    return missingno[k-1]
 
 
 arr = [1, 2, 3, 5, 6, 9, 10, 11, 13, 17]
 k = 3
-find_lastmissingno(arr, k)
+print(find_lastmissingno(arr, k))
+
--- LeetCode.Beginnings/kth_number/tests/test_missing_num.py
+++ LeetCode.Beginnings/kth_number/tests/test_missing_num.py
@@ -2,6 +2,6 @@ from kth_number.missing_num import find_lastmissingno
 
 
 def test_find_lastmissingno():
-    arr = [1, 2, 3, 5, 6, 9, 10, 11, 13, 17]
-    k = 3
+    arr = [-4, -3, 1, 2, 3, 5, 6, 9, 10, 11, 13, 17]
+    k = 4
     assert find_lastmissingno(arr, k) == 12
