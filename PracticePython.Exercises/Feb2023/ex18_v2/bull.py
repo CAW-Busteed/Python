10ddbd73edcb0ecb26df74075186ca26292d26ec
Roy Mathew <rmathew8@gmail.com>
2023-03-07 17:43:19 -0500

    clean up code.

--- PracticePython.Exercises/Feb2023/ex18_v2/bull.py
+++ PracticePython.Exercises/Feb2023/ex18_v2/bull.py
@@ -1,54 +1,51 @@
 '''
-Cows and Bulls
+To get cows & bulls
 
-Randomly generate a 4-digit number. 
-Ask the user to guess a 4-digit number. 
-For every digit that the user guessed correctly in the correct place, they have a “cow”. 
-For every digit the user guessed correctly in the wrong place is a “bull.” 
-Every time the user makes a guess, tell them how many “cows” and “bulls” they have. 
-Once the user guesses the correct number, the game is over. 
-Keep track of the number of guesses the user makes throughout the game and tell the user at the end.
+bullscows <- [] 
+generated_list <- [A,B,C,D]
+inputed_value <- [a,b,c,d]
 
-generated (n) is array of digits
+for each number(i,n) in generated_list:
+    for each number(j,m) in inputed_value:
+        if n == m:
+            if i == j:
+                add 'cow' to bullscows
+                break
+            add 'bull' to bullscows
+            break
+return bullscows
+
+
+
+introduce main loop
+    generate a random 4 digit number
+    game loop begins
+        request input from user
+        compare input to generation and ID no. of cows and bulls
+        if no. of cows is 4, 
+            signal victory and break
+            otherwise loop restarts
 '''
 import random
 
 
-def get_cows(actual, guess):
-    """
-    this function computes the # of cows, returns actual list of cows
-    and actual reduced version (with cows removed) of the input arrays
-    """
-    return None
+# To get cows & bulls:
+def get_cowandbull(gen,input):
+    c_and_b = []
+    for x,i in gen:
+        for y,j in input:
+            if x == y:
+                if i== j:
+                    c_and_b.append('cow')
+                    break
+                c_and_b.append('bull')
+                break
+    return c_and_b
 
 
-def get_bulls(actual, guess):
-    "find matching bulls"
-    return None
-
-
-def get_cows_and_bulls(actual, guess):
-    return None
-
-
-#main loop
 if __name__ == "__main__":
-    # generate a random 4-digit num.
-    num_ = [
-        random.randint(0, 9),
-        random.randint(0, 9),
-        random.randint(0, 9),
-        random.randint(0, 9)
-    ]
+    generated_num = [ random.int(0,9),random.int(0,9),random.int(0,9),random.int(0,9) ]
 
-    tally = 0  # tally is how many user attempts
-    #run = True # flag to terminate main loop.
-
-    #explanation and start of loop
-    print(
-        'A return of a cow means a correct number and correct place. A bull is a correct guess in the wrong place.'
-    )
-    # while True:
-    #     #user input
-    #     ans_ = input('Guess a 4 digit number: ')
-    # return None
+    print('A cow means a correct placement and correct number. Bull just means a correct number')
+    answer = input()
+    [int(x) for x in answer]
--- PracticePython.Exercises/Feb2023/ex18_v2/tests/test_bull.py
+++ PracticePython.Exercises/Feb2023/ex18_v2/tests/test_bull.py
@@ -1,23 +1,7 @@
-from ex18_v2.bull import get_cows_and_bulls
-from ex18_v2.bull import get_bulls
-from ex18_v2.bull import get_cows
+from ex18.bull import get_cows_and_bulls
 
 
-def test_get_cows():
+def test_get_cowandbull():
     a = [1, 2, 3, 4]
     b = [3, 2, 3, 3]
-    assert get_cows(a, b) == ([2, 3], [[1, 4], [3, 3]])
-
-
-def test_get_bulls():
-    a = [1, 4, 3, 2]
-    b = [3, 2, 3, 3]
-    assert get_bulls(a, b) == [3, 2]
-    b = [2, 8, 7, 9]
-    assert get_bulls(a, b) == [2]
-
-
-def test_cows_and_bulls():
-    a = [1, 2, 3, 4]
-    b = [4, 2, 3, 3]
-    assert get_cows_and_bulls(a, b) == [[2, 3], [4]]
+    assert get_cows_and_bulls(a, b) == ([2, 3], [[1, 4], [3, 3]])
