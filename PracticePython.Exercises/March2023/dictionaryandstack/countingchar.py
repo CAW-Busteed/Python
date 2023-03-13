8ab7810c14808b8bc90879703071c53d1b0e54a9
CAW Busteed <acedric1@gmail.com>
2023-03-13 17:09:46 -0400

    updated projects

---
+++ PracticePython.Exercises/March2023/dictionaryandstack/countingchar.py
@@ -0,0 +1,6 @@
+'''count instances of letters in strings of words
+chars<-empty dictionary
+for every word in a list, split into letters
+for every letter:
+    if the letter is already in the dictionary, add one to its value
+    otherwise, add it to the dictionary with a value of one'''
---
+++ PracticePython.Exercises/March2023/dictionaryandstack/openandclosepara.py
@@ -0,0 +1,4 @@
+'''
+for every open bracket there must be a closed bracket
+TODO
+'''
