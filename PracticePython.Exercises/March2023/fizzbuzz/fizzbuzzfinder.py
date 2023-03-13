ad6baa3c012af2caa0da15cb735c3c7a833aec87
CAW Busteed <acedric1@gmail.com>
2023-03-13 16:18:29 -0400

    fixed so it would start from 1

--- PracticePython.Exercises/March2023/fizzbuzz/fizzbuzzfinder.py
+++ PracticePython.Exercises/March2023/fizzbuzz/fizzbuzzfinder.py
@@ -13,15 +13,13 @@ def find_fizzbuzz(upto):
     # and factors of fifteen (‘fizzbuzz’)
     fizzbuzz = []
 
-    for num in range(upto):
+    for num in range(1,upto):
         if num % 15 == 0:
             #If the number is divisible by 15, add ‘fizzbuzz’ to fizzbuzz
             fizzbuzz.append('fizzbuzz')
         #since 15 is divisble by both 5 and 3, it had to be done first
-        else:
-            if num % 3 == 0:
-                fizzbuzz.append('fizz')
-            elif num % 5 == 0:
-                fizzbuzz.append('buzz')
+        elif num % 3 == 0:
+            fizzbuzz.append('fizz')
+        elif num % 5 == 0:
+            fizzbuzz.append('buzz')
     return fizzbuzz
-
