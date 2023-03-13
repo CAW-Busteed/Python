45794a7d08ff4640dbc3d038aaf690dd5e4df675
CAW Busteed <acedric1@gmail.com>
2023-03-13 11:13:31 -0400

    tests come up with mistakes

---
+++ PracticePython.Exercises/March2023/fizzbuzz/fizzbuzzfinder.py
@@ -0,0 +1,27 @@
+'''fizzbuzz<- [] #strings denoting factors of three(’fizz’), factors of five (‘buzz’), and factors of fifteen (‘fizzbuzz’)
+
+For a number in a set range (1-100):
+	If the number is divisible by 15, add ‘fizzbuzz’ to fizzbuzz
+	else:
+If the number is divisible by 3, add ‘fizz’ to fizzbuzz
+		If the number is divisible by 5, add ‘buzz’ to fizzbuzz'''
+
+
+
+def find_fizzbuzz(upto):
+    #result strings denoting factors of three(’fizz’), factors of five (‘buzz’), 
+    # and factors of fifteen (‘fizzbuzz’)
+    fizzbuzz = []
+
+    for num in range(upto):
+        if num % 15 == 0:
+            #If the number is divisible by 15, add ‘fizzbuzz’ to fizzbuzz
+            fizzbuzz.append('fizzbuzz')
+        #since 15 is divisble by both 5 and 3, it had to be done first
+        else:
+            if num % 3 == 0:
+                fizzbuzz.append('fizz')
+            elif num % 5 == 0:
+                fizzbuzz.append('buzz')
+    return fizzbuzz
+
