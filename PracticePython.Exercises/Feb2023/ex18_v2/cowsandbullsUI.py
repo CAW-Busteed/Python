b3ed81e458c76f4cd330280b6954569cb6f49373
CAW Busteed <acedric1@gmail.com>
2023-03-29 17:19:22 -0400

    finish

---
+++ PracticePython.Exercises/Feb2023/ex18_v2/cowsandbullsUI.py
@@ -0,0 +1,86 @@
+'''
+Build a UI for cows and bulls
+four squares that fill with cows and bulls
+'''
+import pygame as pg
+import bull
+import time
+
+SCREEN_WIDTH = 1600
+SCREEN_HEIGHT = 800
+
+def main():
+    def center_num(input, length):
+        if pg.font:
+            font = pg.font.Font(None, length)
+            text = font.render(input, True, (10, 10, 10))
+            #transform font by half of its size from the midpoint of the rect
+            #TODO: make the rest of the numbers in squares, maybe as a function
+            textpos = (100+ (length//2) -(font.size(input)[0]//2)), (100+ (length//2) -(font.size(input)[1]//2)) 
+            #, (400,100), (700,100), (1000,100)
+            #text.get_rect(centerx=screen.get_width() / 2, y=10)
+            screen.blit(text, textpos)
+            pg.display.flip()
+            
+
+    pg.init()
+    # set up a dark green screen with caption
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill((126,200,80))
+    pg.display.set_caption("Cows & Bulls")
+    rect_side = (SCREEN_HEIGHT //3 )
+    clock = pg.time.Clock()
+    
+    #make 4 squares in a row 
+    #draw rect  on screen   black   location        size        border size
+    pg.draw.rect(screen, (0,0,0), (100, 100, rect_side, rect_side), 4)
+    pg.draw.rect(screen, (0,0,0), (400, 100, rect_side, rect_side), 4)
+    pg.draw.rect(screen, (0,0,0), (700, 100, rect_side, rect_side), 4)
+    pg.draw.rect(screen, (0,0,0), (1000, 100, rect_side, rect_side), 4)
+
+    #updates screen
+    pg.display.flip()
+    
+
+    #TODO:collate the bulls program to connect results to 4 squares
+    run = True
+    input = None
+
+    while run:
+        clock.tick(60)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                run = False
+            elif event.type == pg.KEYDOWN and event.key == pg.K_0:
+                center_num('0', rect_side)
+            elif event.type == pg.KEYDOWN and event.key == pg.K_1:
+                center_num('1', rect_side)
+            elif event.type == pg.KEYDOWN and event.key == pg.K_2:
+                center_num('2', rect_side)
+            elif event.type == pg.KEYDOWN and event.key == pg.K_3:
+                center_num('3', rect_side)
+            elif event.type == pg.KEYDOWN and event.key == pg.K_4:
+                center_num('4', rect_side)
+            elif event.type == pg.KEYDOWN and event.key == pg.K_5:
+                center_num('5', rect_side)
+            elif event.type == pg.KEYDOWN and event.key == pg.K_6:
+                center_num('6', rect_side)
+            elif event.type == pg.KEYDOWN and event.key == pg.K_7:
+                center_num('7', rect_side)
+            elif event.type == pg.KEYDOWN and event.key == pg.K_8:
+                center_num('8', rect_side)
+            elif event.type == pg.KEYDOWN and event.key == pg.K_9:
+                center_num('9', rect_side)
+            else:
+                center_num('X', rect_side)
+        time.sleep(5)
+        
+        
+        
+            
+
+if __name__ == "__main__":
+    main()
